573dbc6c8e8e505409ed011e4315d4a4c5b4fb51
Michelle Noorali <michellemolu@gmail.com>
2016-02-18 14:20:16 -0700

    add /healthz endpoint
    * update makefile commands with new tag

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	go build .
 
 image:
-	docker build -t mnoorali/puffy:1.0.0 .
+	docker build -t mnoorali/puffy:2.0.0 .
 
 push:
-	docker push mnoorali/puffy:1.0.0
+	docker push mnoorali/puffy:2.0.0
--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func indexHandler(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "Hello world from Puffy!")
 }
 
+func healthHandler(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "Don't worry. Puffy is fine!\n")
+}
+
 func main() {
 	http.HandleFunc("/index", indexHandler)
+	http.HandleFunc("/healthz", healthHandler)
 	http.ListenAndServe(":8080", nil)
 }
